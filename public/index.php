af2eb73907379f07ad9eca602f8ecdf85ee701b7
Volkan Yavuz <vkn@vknyvz.com>
2012-11-04 18:30:24 -0500

    setup

--- public/index.php
+++ public/index.php
@@ -3,9 +3,24 @@ defined('BASE_PATH') || define('BASE_PATH', realpath(dirname(__FILE__)));
 defined('APPLICATION_PATH') || define('APPLICATION_PATH', BASE_PATH . '/../app');
 defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 
-set_include_path(implode(PATH_SEPARATOR, array(realpath(APPLICATION_PATH . '/../library'), get_include_path(), )));
-   
-require_once 'Zend/Application.php';
+switch(APPLICATION_ENV) {
+	case 'development':
+		set_include_path(implode(PATH_SEPARATOR, array(realpath(APPLICATION_PATH . '/../library'), get_include_path(), )));
+		
+		require_once 'Zend/Application.php';
+		
+		define('SERVER_PATH', '/var/www/gym.com');
+		break;	
+	case 'production':
+	case '':		
+		set_include_path(implode(PATH_SEPARATOR, array(realpath(APPLICATION_PATH . '/../../../'), get_include_path(), )));
+		set_include_path(implode(PATH_SEPARATOR, array(realpath(APPLICATION_PATH . '/../library'), get_include_path(), )));
+		
+		require_once 'Zend/Application.php';
+		
+		define('SERVER_PATH', '/home2/volkanan/www/_sub/gym');
+		break;	
+}
 
 $application = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/config.xml');
 $application->bootstrap()->run();
